19dbbd61317d3ada394444ec9268b1101a462ca1
KinGFelipealves <luizfelipealvesti@gmail.com>
2025-09-09 14:29:56 -0300

    CartasSuperTrunfo.c
    Nível Aventureiro

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,3 @@
-
-
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
-
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
-
 #include <stdio.h>
 
 int main(){
@@ -26,7 +7,7 @@ int main(){
     char carta [4];
     char cidade [25];
     int populaçao, pontos_turisticos;
-    float PIB, area;
+    float PIB, area,densidade_populacional, PIB_per_capita;
 
     // CARTA 2
     
@@ -34,7 +15,7 @@ int main(){
     char carta2 [4];
     char cidade2 [25];
     int populaçao2, pontos_turisticos2;
-    float PIB2, area2;
+    float PIB2, area2, densidade_populacional2, PIB_per_capita2;
 
    // Carta 1:
 //  Estado: A
@@ -44,7 +25,8 @@ int main(){
 //  Área: 1521.11 km²
 //  PIB: 699.28 bilhões de reais
 //  Número de Pontos Turísticos: 50
-
+//  Densidade Populacional: 8102.47 hab/km²
+//  PIB per Capita: 56724.32 reais
 
     // Carta 2:
 //  Estado: B
@@ -54,7 +36,8 @@ int main(){
 //  Área: 1200.25 km²
 //  PIB: 300.50 bilhões de reais
 //  Número de Pontos Turísticos: 30
-
+//  Densidade Populacional: 5622.24 hab/km²
+//  PIB per Capita: 44532.91 reais
 
     printf ("Digite o Estado: \n");
     scanf (" %c", &estado);
@@ -76,6 +59,20 @@ int main(){
 
     printf("Digite a quantidade de Pontos Turísticos:\n");
     scanf("%d", &pontos_turisticos);
+
+    printf("Digite a Densidade Populacional:\n");
+    scanf("%f", &densidade_populacional);
+    
+    printf("Digite o PIB per Capita:\n");
+    scanf("%f", &PIB_per_capita);
+
+    densidade_populacional = populaçao / area;
+    printf("Densidade populacional: %.2f hab/km²\n", densidade_populacional );
+    
+    PIB_per_capita = PIB * 1000000000 / populaçao;
+    printf("Pib Per Capita: %.2f reais\n", PIB_per_capita );
+
+
 
     // CARTA 2
 
@@ -100,6 +97,20 @@ int main(){
     printf("Digite a quantidade de Pontos Turísticos:\n");
     scanf("%d", &pontos_turisticos2);
 
+    printf("Digite a Densidade Populacional:\n");
+    scanf("%f", &densidade_populacional2);
+
+    printf("Digite o PIB per Capita:\n");
+    scanf("%f", &PIB_per_capita2);
+    
+    densidade_populacional2 = populaçao2 / area2;
+    printf("Densidade populacional: %.2f Hab/km²\n", densidade_populacional2 );
+    
+    PIB_per_capita2 = PIB2 * 1000000000 / populaçao2;
+    printf("Pib Per Capita: %.2f reais\n", PIB_per_capita2  );
+
+
+
     /////////////////////////////////////////////////////////////
 
     printf("\n\n---INFORMAÇÕES DOS DADOS--- \n");
@@ -111,7 +122,10 @@ int main(){
     printf("Área: %.2f km²\n", area);
     printf("PIB: %.2f Bilhões de Reais\n", PIB);
     printf("Pontos Turisticos: %d\n", pontos_turisticos );
-
+    printf("Densidade Populacional:%.2f\n", densidade_populacional);
+    printf("PIB Per Capital: %.2f \n", PIB_per_capita);
+    
+   
     ///////////////////////////////////////////////////////////////
 
  printf("\n\n---INFORMAÇÕES DOS DADOS--- \n");
@@ -123,7 +137,9 @@ int main(){
     printf("Área: %.2f km²\n", area2);
     printf("PIB: %.2f Bilhões de Reais\n", PIB2);
     printf("Pontos Turisticos: %d\n", pontos_turisticos2 );
-
+    printf("Densidade Populacional:%.2f\n", densidade_populacional2);
+    printf("PIB Per Capital: %.2f \n", PIB_per_capita2);
+   
     return 0;
     
 }
